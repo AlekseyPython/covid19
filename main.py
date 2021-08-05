f7a0db88e585e332f6abf51cb675e01eb183a35f
AlekseyPython <88241817+AlekseyPython@users.noreply.github.com>
2021-08-05 12:40:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import Settings, Initialization
+import warnings
+
+
+warnings.simplefilter(action='ignore')
+
+if __name__ == '__main__':
+    if Settings.console_mode:
+        from Business.ConvertDataToDesiredTypesTask import Task
+        
+        task = Task()
+        task.start()
+        task.join()
+    else:
+        ipresentation = Initialization.ipresentation
+        ipresentation.build_mian_window()
